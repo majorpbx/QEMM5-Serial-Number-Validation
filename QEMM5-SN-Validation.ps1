7c56f0292e6150cb30d9bea3b7b7cf8417d3276d
majorpbx <18235905+majorpbx@users.noreply.github.com>
2023-06-11 12:56:09 -0400

    Update QEMM5-SN-Validation.ps1
    Aligned with de-compiled assembly code.

--- QEMM5-SN-Validation.ps1
+++ QEMM5-SN-Validation.ps1
@@ -1,21 +1,56 @@
-# $SN = "328-B6-7676" # Good Serial Number
-# $SN = "328-XQ-7676" # Good Serial Number
-$SN = "000-B6-0000" # Bad Serial Number
+#############################################################
+#                                                           #
+# Version 5:                                                #
+#           "328-B6-7676" Good Serial Number                #
+#           "328-XQ-7676" Good Serial Number                #
+#           "000-B6-0000" Bad Serial Number                 #
+#                                                           #
+# Version 7:                                                #
+#           "500-P7R-4152" Good Serial Number               #
+#           "050-ì7@-4152" Good Serial Number               #
+#           "050-@7í-4152" Bad Serial Number                #
+#                                                           #
+#############################################################
 
-If ($SN[3] -ne "-") { return Write-Host "SN: $($SN) Bad.... Try Again" }
-If ($SN[6] -ne "-") { return Write-Host "SN: $($SN) Bad.... Try Again" }
+# Re-wrote this into a function and aligned the code to follow de-compiled assembly code.
+#
+# Working on adding more versions but for now only truely supports v5.
 
-Switch ("$($SN[4])$($SN[5])") {
-   "B6" { break }
-   "XQ" { break }
-default { return Write-Host "SN: $($SN) is Bad.... Try Again" }
-}
+Function QD-SerialCheck {
+Param (
+[Parameter(Mandatory=$False, ValueFromPipeline=$true)]
+# [ValidateScript({Test-Path $_})]
+[String]$SerialNumber,
+[Parameter(Mandatory=$false, ValueFromPipeline=$true)]
+[ValidateSet("5","6","7","8")]
+[String]$Version = "5"
+)
 
 $BL = 10
+
+
+If ($SerialNumber[3] -ne "-") { return Write-Host "SN: $($SerialNumber) Bad.... Try Again" }
+If ($SerialNumber[6] -ne "-") { return Write-Host "SN: $($SerialNumber) Bad.... Try Again" }
+
+If ("$($SerialNumber[4])$($SerialNumber[5])" -eq "XQ") {  } Else { 
+
+If ($SerialNumber[4] -ne "B") { return Write-Host "SN: $($SerialNumber) Bad.... Try Again" }
+If ($SerialNumber[5] -ne "6") { return Write-Host "SN: $($SerialNumber) Bad.... Try Again" }
+
+}
+<#
+Switch ("$($SerialNumber[4])$($SerialNumber[5])") {
+   "B6" { break }
+   "XQ" { break }
+default { return Write-Host "SN: $($SerialNumber) is Bad.... Try Again" }
+}
+#>
+
 $Data = $NULL
 
 For ($c=0;$c -le 10;$c++) { 
-If (($c -le 2) -or ($c -ge 8)) { $Data += $([Convert]::ToInt16($SN[$c],10)) }
+$Data
+If (($c -le 2) -or ($c -ge 8)) { $Data += $([Convert]::ToInt16($SerialNumber[$c],10)) }
 }
 
 $AL = [System.Math]::Round($($Data) / $($BL))
@@ -23,8 +58,14 @@ $AH = $($Data) % $($BL)
 $BL = ($($BL) - 1) - $($AH)
 $AH = $($BL) + 30
 
-If ($($AH) -eq $("{0:X2}" -f [int][char]$SN[7])) {
-Write-Host "SN: $($SN) is Good!"
+If ($($AH) -eq $("{0:X2}" -f [int][char]$SerialNumber[7])) {
+
+return Write-Host "SN: $($SerialNumber) is Good!"
+
 } Else {
-Write-Host "SN: $($SN) is Bad.... Try again"
+
+return Write-Host "SN: $($SerialNumber) is Bad.... Try again"
+
+}
+
 }
